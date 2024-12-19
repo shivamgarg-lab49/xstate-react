eaa20c588cd0f6f7dca27394a29d1528045e82cc
Shivam Garg <shivam.garg@lab49.com>
2024-12-19 12:31:28 +0530

    added tailwind
    Signed-off-by: Shivam Garg <shivam.garg@lab49.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <p>Welcome</p>;
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-900">Hello world!</h1>
+  );
 }
-
-export default App;
