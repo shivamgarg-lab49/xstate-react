6e22dd2413b5f9c33298150234c46b4da9483967
Shivam Garg <shivam.garg@lab49.com>
2024-12-19 14:00:50 +0530

    most basic login machine written

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { LoginComponent } from "./components/LoginComponent";
+import Sample from "./components/Sample";
+import { LoginMachineContext } from "./machines/login.machine";
 
 export default function App() {
   return (
-    <>
+    <LoginMachineContext.Provider>
       <LoginComponent />
-    </>
+      <Sample />
+    </LoginMachineContext.Provider>
   );
 }
--- src/machines/login.machine.ts
+++ src/machines/login.machine.ts
@@ -1,14 +1,63 @@
-import { createMachine } from "xstate";
+import { setup, fromPromise, log } from "xstate";
+import { User } from "../types/types";
+import { createActorContext } from "@xstate/react";
 
-export const loginMachine = createMachine({
-  id: "toggle",
-  initial: "Inactive",
+type LoginEvent = {
+  type: "Check User State";
+};
+
+export const loginMachine = setup({
+  types: {} as {
+    events: LoginEvent;
+  },
+  actors: {
+    checkIfUserIsAlreadySaved: fromPromise<User>(() => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const user = localStorage.getItem("savedUser");
+          if (user !== null && user !== undefined) {
+            resolve(JSON.parse(user));
+          } else {
+            reject("No user saved");
+          }
+        }, 1000);
+      });
+    }),
+  },
+}).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QBsD2UCWA7AsgQwGMALbMAOgAlUBbMAYgGEiwCBrAAgFVYwAndgMoAXPELABtAAwBdRKAAOqWBiEZUWOSAAeiAIwBmABxkAbABZJJ-QCYT1gJz77ZgKz6ANCACee65LLWLgC+QZ5omLiEJFjkTCwcAngAbpBcPLx0EOrk2EmorOTh2PjEpGRxbILJqdx8CLmoBKJqWFLSbZqKyqrqmjoIAOyuZLoDuoaGtg5Orp4+CLqS1gHBoSBFkaUx5cyViSkQaXx0fLyovGTyyKIAZufUZBsl0bG7CdWHtbz1WHlNPa0ZB0kCAuioWn1EEMXCMxhMpo5nC45noTP4hvZ7IYzENdNZFs4QmssKgIHBNE8oqROkpwb0Qf0ALQmFEIRkwzGcyS6Mw8-QuQwDfQhMLoYpU7ZUWg07oQhmIMzWVl49GrUURZ5lCrvA5HXgyukaeUIGzGUbjSZ2RGuDzeRCGXRkNXrMWbF5kL7sACiWgwsCEBoBkIWS30Tt0LisCJmyLtIcdgRFLo1EvInpJQm9vv9gbloH6i2sYZcEajVpjypc9jIA1WISAA */
+  id: "loginMachine",
+  initial: "Home",
   states: {
-    Inactive: {
-      on: { toggle: "Active" },
+    Home: {
+      on: {
+        "Check User State": {
+          actions: log("Checking User State event has been fired"),
+          target: "Check Saved User",
+        },
+      },
     },
-    Active: {
-      on: { toggle: "Inactive" },
+
+    "Check Saved User": {
+      tags: ["state-loading"],
+
+      description:
+        "Here in this state we are checking if user is already logged in",
+
+      invoke: {
+        src: "checkIfUserIsAlreadySaved",
+        onDone: {
+          target: "User Exist",
+        },
+        onError: {
+          target: "User not Exist",
+        },
+      },
     },
+
+    "User Exist": {},
+    "User not Exist": {},
   },
 });
+
+export const LoginMachineContext = createActorContext(loginMachine);
